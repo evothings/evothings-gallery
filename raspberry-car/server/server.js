a76692d13e0dcb496ec06f4ae71a849c403ee013
Eric Svensson <eric@ericsvensson.se>
2014-07-01 13:59:51 +0200

    Raspberry Car: removed semicolons and did minor reformatting for improved readability.

--- raspberry-car/server/server.js
+++ raspberry-car/server/server.js
@@ -6,75 +6,80 @@ Copyright (c) 2014 Evothings AB
 */
 
 // Start a socket.io server that listens on port 8085.
-var io = require('/opt/node/lib/node_modules/socket.io').listen(8085, { log: false });
+var io = require('/opt/node/lib/node_modules/socket.io').listen(8085, { log: false })
 
 // Load required modules.
 var sys = require('sys'),
-	exec = require('child_process').exec;
+	exec = require('child_process').exec
 
-// Path to Raspbian's gpio driver, used for sending signals to the reote.
+// Path to Raspbian's gpio driver, used for sending signals to the remote.
 var path = '/sys/class/gpio/',
 // Pin numbers on the Raspberry Pi connected to the car's remote.
 	pins = [7, 8, 9, 11];
 
 // Enable sending signals to the car's remote control
 // which is connected to the Raspberry Pi.
-initPins();
+initPins()
 
 // Listen for connections
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket)
+{
 
 	// Listen for direction messages from the app.
-	socket.on('direction', function (data) {
-		console.log('received direction: ' + data);
+	socket.on('direction', function (data)
+	{
+		console.log('received direction: ' + data)
 
 		// Choose the right command for sending a signal to the car's remote.
-		command = '';
-		if ( data === 'UP')
-			command = 'echo 1 > ' + path + 'gpio7/value';
-		else if ( data === 'DOWN')
-			command = 'echo 1 > ' + path + 'gpio8/value';
-		else if ( data === 'LEFT')
-			command = 'echo 1 > ' + path + 'gpio9/value';
-		else if ( data === 'RIGHT')
-			command = 'echo 1 > ' + path + 'gpio11/value';
-		else if ( data === 'STOPUP')
-			command = 'echo 0 > ' + path + 'gpio7/value';
-		else if ( data === 'STOPDOWN')
-			command = 'echo 0 > ' + path + 'gpio8/value';
-		else if ( data === 'STOPLEFT')
-			command = 'echo 0 > ' + path + 'gpio9/value';
-		else if ( data === 'STOPRIGHT')
-			command = 'echo 0 > ' + path + 'gpio11/value';
+		command = ''
+		if ( data === 'UP' )
+			command = 'echo 1 > ' + path + 'gpio7/value'
+		else if ( data === 'DOWN' )
+			command = 'echo 1 > ' + path + 'gpio8/value'
+		else if ( data === 'LEFT' )
+			command = 'echo 1 > ' + path + 'gpio9/value'
+		else if ( data === 'RIGHT' )
+			command = 'echo 1 > ' + path + 'gpio11/value'
+		else if ( data === 'STOPUP' )
+			command = 'echo 0 > ' + path + 'gpio7/value'
+		else if ( data === 'STOPDOWN' )
+			command = 'echo 0 > ' + path + 'gpio8/value'
+		else if ( data === 'STOPLEFT' )
+			command = 'echo 0 > ' + path + 'gpio9/value'
+		else if ( data === 'STOPRIGHT' )
+			command = 'echo 0 > ' + path + 'gpio11/value'
 
 		// Execute the command to send the signal to the car's remote.
-		exec(command, function (error, stdout, stderr) {
+		exec(command, function (error, stdout, stderr)
+		{
 			if (error !== null)
-				console.log('exec error: ' + error);
-		});
+				console.log('exec error: ' + error)
+		})
 
-	});
+	})
 
-});
+})
 
-function initPins() {
+function initPins()
+{
 	// Enable control of the Raspberry Pi's gpio pins.
 	// Read more at http://elinux.org/RPi_Low-level_peripherals#Bash_shell_script.2C_using_sysfs.2C_part_of_the_raspbian_operating_system
 	for (var pin in pins)
 	{
-		console.log('Creating port ' + pins[pin] + '...');
+		console.log('Creating port ' + pins[pin] + '...')
 
 		// The command first checks whether the port already exists.
 		var command = 'if (! [ -f ' + path + 'gpio' + pins[pin] + '/direction ]); then ' +
 			'echo ' + pins[pin] + ' > ' + path + 'export; fi';
 
 		// Create the ports using Raspbian's command line.
-		exec(command, function(error, stdout, stderr) {
+		exec(command, function(error, stdout, stderr)
+		{
 			if (error === null)
-				console.log('Successfully created port.');
+				console.log('Successfully created port.')
 			else
-				console.log('Error when creating port: ' + error + ' (' + stderr + ').');
-		});
+				console.log('Error when creating port: ' + error + ' (' + stderr + ').')
+		})
 	}
 
 	// Configure the Raspberry Pi's gpio pins as output ports which enables signals
@@ -82,17 +87,18 @@ function initPins() {
 	// Read more at http://elinux.org/RPi_Low-level_peripherals#Bash_shell_script.2C_using_sysfs.2C_part_of_the_raspbian_operating_system
 	for (var pin in pins)
 	{
-		console.log('Configuring port ' + pins[pin] + '...');
+		console.log('Configuring port ' + pins[pin] + '...')
 
 		// The command configures the pin as an output port.
-		var command = 'echo out > ' + path + 'gpio' + pins[pin] + '/direction';
+		var command = 'echo out > ' + path + 'gpio' + pins[pin] + '/direction'
 
 		// Configure the ports using Raspbian's command line.
-		exec(command, function(error, stdout, stderr) {
+		exec(command, function(error, stdout, stderr)
+		{
 				if (error === null)
-					console.log('Successfully configured pin.'); 
+					console.log('Successfully configured pin.')
 				else
-					console.log('Error when configuring port: ' + error + ' (' + stderr + ').');
-		});
+					console.log('Error when configuring port: ' + error + ' (' + stderr + ').')
+		})
 	}
 }
