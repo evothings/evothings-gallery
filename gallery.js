f506a507530b90acb24a4f4f2a7748b2613afaeb
Eric Svensson <eric@ericsvensson.se>
2014-08-13 13:42:35 +0200

    Added method for changing app URLs protocol to evothings:// through supplying a querystring. Removed semicolons in JavaScript code.

--- gallery.js
+++ gallery.js
@@ -5,100 +5,106 @@ Author: Eric Svensson
 Copyright (c) 2013-2014 Evothings AB
 */
 
-$(function() {
+;$(function() {
 
 	$.getJSON("gallery.json", function(data) {
 
-		var $list = $("#list");
-		var $listItemTemplate = $("#list_item_template");
-		var $resourceTemplate = $("#list_item_template .resource");
+		var $list = $("#list")
+		var $listItemTemplate = $("#list_item_template")
+		var $resourceTemplate = $("#list_item_template .resource")
 
-		$.each(data.items, function(key, val) {
+		$.each(data.items, function(key, item) {
 
 			/*  If the query string specifies that only items with certain
 				tags should be shown and the current item doesn't contain those
 				tags then continue to the next item. */
-			if (val.tags && $.QueryString["showtags"] &&
+			if (item.tags && $.QueryString["showtags"] &&
 				($.arrayIntersect(
-					val.tags.split(','),
+					item.tags.split(','),
 					$.QueryString["showtags"].split(','))
 				).length == 0)
-				return true; // same as 'continue' in a native JS loop
+				return true // same as 'continue' in a native JS loop
 
 			/*  If the query string specifies that only items lacking certain
 				tags should be shown and the current item contains one or more
 				of those tags then continue to the next item. */
-			if (val.tags && $.QueryString["hidetags"] &&
+			if (item.tags && $.QueryString["hidetags"] &&
 				($.arrayIntersect(
-					val.tags.split(','),
+					item.tags.split(','),
 					$.QueryString["hidetags"].split(','))
 				).length > 0)
-				return true; // same as 'continue' in a native JS loop
+				return true // same as 'continue' in a native JS loop
 
 			var $newItem = $listItemTemplate
 				.clone()
 				.appendTo($list)
 				.attr("id", "")
-				.addClass("visible");
+				.addClass("visible")
+
+			/*  If supplying the 'etprotocol' querystring (e.g. when
+				navigating from Evothings Client, prepend evothings:// to the
+				app URL so that the apps open in the Evothings Client app. */
+			if (item.url && $.QueryString["etprotocol"])
+				item.url = item.url.replace(/https?:\/\//, 'evothings://')
 
 			$newItem
 				.children("a.screenshot")
-					.attr("href", val.url)
+					.attr("href", item.url)
 					.children("img")
-						.attr("src", val.image)
-						.attr("alt", val.description)
+						.attr("src", item.image)
+						.attr("alt", item.description)
 						.end()
 					.children("div.description")
-						.text(val.description)
-						.end();
+						.text(item.description)
+						.end()
 
 			$newItem
 				.find(".author")
-				.text(val.author);
+				.text(item.author)
 
-			if (val.links)
-				$.each(val.links, function(resourceKey, resourceVal) {
+			if (item.links)
+				$.each(item.links, function(resourceKey, resourceVal) {
 					$newItem.append(
 						$resourceTemplate
 							.clone()
-							.addClass("resource-"+resourceKey)
+							.addClass("resource-" + resourceKey)
 							.attr("href", resourceVal)
 							.text(resourceKey)
 							.show()
-					);
-				});
-		});
+					)
+				})
+		})
 
 	}).fail(function() {
 		alert('Failed to load gallery.')
-	});
+	})
 
-});
+})
 
 /* jQuery plug-in that parses the URL Query String.
    Usage example: get the query string parameter named "param" through
    $.QueryString["param"] */
-(function($) {
+;(function($) {
 	$.QueryString = (function(a) {
-		if (a == "") return {};
-		var b = {};
+		if (a == "") return {}
+		var b = {}
 		for (var i = 0; i < a.length; ++i)
 		{
-			var p=a[i].split('=');
+			var p = a[i].split('=')
 			if (p.length != 2) continue;
-			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+			b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "))
 		}
 		return b;
 	})(window.location.search.substr(1).split('&'))
-})(jQuery);
+})(jQuery)
 
 /* jQuery plug-in that produces the intersection between the two input arrays. */
-(function($) {
+;(function($) {
 	$.arrayIntersect = function(a, b)
 	{
 		return $.grep(a, function(i)
 		{
-			return $.inArray(i, b) > -1;
-		});
+			return $.inArray(i, b) > -1
+		})
 	};
-})(jQuery);
+})(jQuery)
